b340d0c88ab2da4403b146f0f7e52565f43df6e1
Aloísio Carvalho <loistc.carvalho@gmail.com>
2023-01-12 20:31:41 -0300

    exemplo do padrao bridge

--- estructural-patterns/src/main/java/br/com/cod3r/bridge/converter/Client.java
+++ estructural-patterns/src/main/java/br/com/cod3r/bridge/converter/Client.java
@@ -1,8 +1,23 @@
 package br.com.cod3r.bridge.converter;
 
+import br.com.cod3r.bridge.converter.converters.CSVConverter;
+import br.com.cod3r.bridge.converter.converters.Converter;
+import br.com.cod3r.bridge.converter.converters.JsonConverter;
+import br.com.cod3r.bridge.converter.employees.ITGuy;
+import br.com.cod3r.bridge.converter.employees.ProjectManager;
+
 public class Client {
 
 	public static void main(String[] args) {
-		
+		Converter csvConverter = new CSVConverter();
+		Converter jsonConverter = new JsonConverter();
+
+		ITGuy it = new ITGuy("Marcio Silva", 34, 6400d);
+		ProjectManager pm = new ProjectManager("Aurelio Morais", 41, 7500d);
+
+		System.out.println(csvConverter.getEmployeeFormated(it));
+		System.out.println(jsonConverter.getEmployeeFormated(it));
+		System.out.println(csvConverter.getEmployeeFormated(pm));
+		System.out.println(jsonConverter.getEmployeeFormated(pm));
 	}
 }
