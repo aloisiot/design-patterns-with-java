d42631bed446258a6904f5f34d3f0cc67e0c5d49
aloisiot <loistc.carvalho@gmail.com>
2023-01-30 21:27:24 -0300

    Exemplo do padrao command

--- behavioral-patterns/src/main/java/br/com/cod3r/command/alexa/alexa/Alexa.java
+++ behavioral-patterns/src/main/java/br/com/cod3r/command/alexa/alexa/Alexa.java
@@ -3,38 +3,32 @@ package br.com.cod3r.command.alexa.alexa;
 import java.util.HashMap;
 import java.util.Map;
 
-import br.com.cod3r.command.alexa.lights.PhillipsHueLight;
-import br.com.cod3r.command.alexa.lights.XiaomiLight;
+import br.com.cod3r.command.alexa.commands.Command;
 
 public class Alexa {
-	private Map<String, Object> integrations;
-	
+	private final Map<String, Command> integrations;
+	private final AlexaIA ia;
 	public Alexa() {
-		integrations = new HashMap<String, Object>();
-		integrations.put("LivingRoomLight", new PhillipsHueLight());
-		integrations.put("KitchenLight", new XiaomiLight());
+		integrations = new HashMap<>();
+		ia = new AlexaIA();
+	}
+
+	public void addIntegration(String key, Command command, String... keywords) {
+		integrations.put(key, command);
+		ia.addAssociation(key, keywords);
 	}
 
 	public void sendRequest(String request) {
-		if(request.equals("Turn on the Living room light")) {
-			System.out.println("Turnning on the Living room light");
-			PhillipsHueLight light = (PhillipsHueLight) integrations.get("LivingRoomLight");
-			light.turnOn();
-		} else if(request.equals("Turn off the Living room light")) {
-			System.out.println("Turnning off the Living room light");
-			PhillipsHueLight light = (PhillipsHueLight) integrations.get("LivingRoomLight");
-			light.turnOff();
-		} else if(request.equals("Turn on the Kitchen light")) {
-			System.out.println("Turnning on the Kitchen light");
-			XiaomiLight light = (XiaomiLight) integrations.get("KitchenLight");
-			light.turnOn();
-		} else if(request.equals("Turn off the Kitchen light")) {
-			System.out.println("Turnning off the Kitchen light");
-			XiaomiLight light = (XiaomiLight) integrations.get("KitchenLight");
-			light.turnOff();
-		} else {
-			System.out.println("Sorry, I can't perform your request!");
+		Command command = integrations.get(request);
+		if(command == null) {
+			String key = ia.getFindAssociations(request);
+			command = integrations.get(key);
 		}
-			 
+		if(command == null) {
+			System.out.println("Sorry, I can't perform your request!");
+		} else {
+			System.out.println("Send a generic command as you saved in my configurations");
+			command.execute();
+		}
 	}
 }
